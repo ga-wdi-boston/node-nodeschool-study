03ce7631c11572edb94fffc4be71e7534430ecb7
saphron <saphron.scul@gmail.com>
2015-11-16 17:33:57 -0500

    Wrote asynchronous function to read file and print number of newlines.

---
+++ learnyounode/03-my-first-async-io.js
@@ -0,0 +1,7 @@
+// Write a program that uses a single asynchronous filesystem operation to read a file and print the number of newlines it contains to the console (stdout), similar to running cat file | wc -l.  The full path to the file to read will be provided as the first command-line argument.
+var fs = require('fs');
+var file = fs.readFile(process.argv[2], function  (err, data) {
+  if (!err) {
+    console.log(data.toString().split('\n').length - 1);
+  }
+});
