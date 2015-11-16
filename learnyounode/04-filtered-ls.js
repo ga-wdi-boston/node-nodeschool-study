6a3df05fa2c1f281fa21d9e2fee70e09aab39f40
Jeff Meyer <jcmeyer10@gmail.com>
2015-11-16 17:27:56 -0500

    "learned a lot about modules and refatoring my filter to pass through the right arguments in order for the module to do the heavy lifting"

--- learnyounode/04-filtered-ls.js
+++ learnyounode/04-filtered-ls.js
@@ -1,10 +1,15 @@
+var mymodule = require('./05-make-it-modular.js');
 var fs = require('fs');
 var path = require('path');
 
-fs.readdir(process.argv[2], function(err, list){
-  list.filter(function(file){
-    if (path.extname(file) === '.'+ process.argv[3]){
-      console.log(file)
-    }
+var dir = process.argv[2];
+var filterExt = process.argv[3];
+
+mymodule(dir, filterExt, function(err, list){
+  if (err)
+    return console.error('There was an error:', err);
+
+  list.forEach(function (file) {
+    console.log(file);
   });
 });
