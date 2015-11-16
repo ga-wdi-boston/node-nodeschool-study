abe8b4f4368540e1a9bf87fd18e33124d4057273
sdavidson140 <sdavidson140@gmail.com>
2015-11-16 16:02:46 -0500

    refactored code to question 3/ my first io
    comment out original code and eliminate bulk

--- learnyounode/02-my-first-io.js
+++ learnyounode/02-my-first-io.js
@@ -1,10 +1,16 @@
 var fs = require('fs'); //require file system module
 //from node library
 
+// var buffer = fs.readFileSync(process.argv[2]);
+
+// var string = buffer.toString();
+
+// var file = string.split('\n');
+
+// console.log(file.length - 1);
+
 var buffer = fs.readFileSync(process.argv[2]);
 
-var string = buffer.toString();
+var lines = buffer.toString().split('\n');
 
-var file = string.split('\n');
-
-console.log(file.length - 1);
+console.log(lines.length - 1);
