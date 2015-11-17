b0d07fd0f7c7f71b8231d939489a8c21e8a913f2
cwcrane <ccrane@excelsiorintegrated.com>
2015-11-16 23:19:31 -0500

    modeule file for question 5

--- learnyounode/05-mymodule.js
+++ learnyounode/05-mymodule.js
@@ -6,7 +6,6 @@ module.exports = function(dirName, extFilter, callback){
   fs.readdir(dirName, function cb(err, list){
     if (err){
       return callback(err, null);
-    //console.log('We\'re getting an error: ' + err);
     }else {
       result = [];
       list.forEach(function(x){
