11d0160ad98641a70d6c435bcdc23c3ddf1d7434
sarabrent <sarabrentmccoy@gmail.com>
2015-11-17 06:57:20 -0500

    Trying later.

--- learnyounode/03-my-first-async-io.js
+++ learnyounode/03-my-first-async-io.js
@@ -1,9 +1,9 @@
 var fs = require('fs');
 
-var buf = fs.readFile(process.argv[2], function(err, data){
+fs.readFile(process.argv[2], 'utf-8', function(err, data){
 	if (err) throw err;
 	else {
-		var lineCount = buf.toString().split('\n').length - 1;
+		var lineCount = data.split('\n').length - 1;
 		console.log(lineCount());
-		};
+	};
 });
