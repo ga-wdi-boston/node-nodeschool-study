328e4fc25d21fb065f31d76b6b90c13d7255e513
bbarber <bbarber@coastalcomputerservices.net>
2015-11-17 18:03:23 -0500

    Ended up using the split and pop methods (vs the path.extname as specified in documentation) to isolate the file extensions.  While this worked for the most it caused an issue with a file with no file name and the filtered extension.  Resolved an issue with an external if else statement.
    May come back to refactor code later & use path.extname and bypass
    the extra if..else code at the end.

--- learnyounode/04-filtered-ls.js
+++ learnyounode/04-filtered-ls.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 fs.readdir(process.argv[2], function(err, list) {
   var fileList = list.filter(function(filename){
     var fileExt = filename.split('.').pop();
-    // console.log(filename);
+
     return fileExt === process.argv[3];
     });
   fileList.forEach(function(fname){
@@ -14,22 +14,3 @@ fs.readdir(process.argv[2], function(err, list) {
     };
   });
 });
-
-
-//   stringArray = [ 'CHANGELOG.md',
-//   'LICENCE.md',
-//   'README.md',
-//   'api.html',
-//   'dat',
-//   'data.dat',
-//   'data.json',
-//   'learnyounode.dat',
-//   'learnyounode.sql',
-//   'learnyounode.txt',
-//   'md',
-//   'w00t.dat',
-//   'w00t.txt',
-//   'words.dat',
-//   'wrrrrongdat' ];
-
-// extFilter(stringArray, "dat", cb);
