cddc66c603b152b315a635937dc268e03c23beb7
Kaylal12 <kaylalei224@gmail.com>
2015-11-16 22:04:49 -0500

    Completed make it modular exercise.

--- learnyounode/04-filtered-ls.js
+++ learnyounode/04-filtered-ls.js
@@ -1,14 +1,21 @@
-var fs = require('fs');
-var path = require('path');
+var fs = require('fs')
+var path = require('path')
 
-var callback = function callback(err, list) {
-  list.forEach(function(file){
-    if (path.extname(file) === '.md')
-      console.log(file);
-  });
-};
+module.exports = function (dir, filterStr, cb) {
 
-fs.readdir(process.argv[2], callback);
+  fs.readdir(dir, function (err, list) {
+    if (err) {
+      return cb(err)
+    }
+
+    list = list.filter(function (file) {
+      return path.extname(file) === '.' + filterStr
+    })
+
+    cb(null, list)
+  })
+}
+
 
 // OFFICIAL SOLUTION
 // var fs = require('fs')
