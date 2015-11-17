2a7d8e63cc8ab5fdbeb88444f9b0513a2eb9787c
christinemoses <cmoses873@gmail.com>
2015-11-16 22:14:18 -0500

    Adding verified solution for learnyounode challenge make-it-modular Corrected indentation for baby-steps and my-first-async-io

--- learnyounode/01-baby-steps.js
+++ learnyounode/01-baby-steps.js
@@ -1,6 +1,6 @@
 var result = 0;
 
-  for (var i = 2; i < process.argv.length; i++){
-    result += Number(process.argv[i]);
+for (var i = 2; i < process.argv.length; i++){
+  result += Number(process.argv[i]);
 }
-  console.log(result);
+console.log(result);
