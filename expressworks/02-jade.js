7acb75e45c357d06b2694642c17410c3896086b9
racheltstevens <racheltstevens@gmail.com>
2015-11-19 17:02:27 -0500

    Adds completed excercise 02
    I figured out that the problem was that I wasn't asking it to render the view in the right place.

--- expressworks/02-jade.js
+++ expressworks/02-jade.js
@@ -2,11 +2,11 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-app.set('views', path.join(process.argv[3], 'templates'));
+app.set('views', path.join(__dirname, 'templates'));
 app.set('view engine', 'jade');
 app.get('/home', function(req,res){
-  res.render('index', {date: new Date().toDateString()})
-})
+  res.render(process.argv[3], {date: new Date().toDateString()});
+});
 
 
-app.listen(process.argv[2])
+app.listen(process.argv[2]);
