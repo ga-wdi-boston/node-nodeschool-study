b4b76be8605b09e5e065d1cc717e48e3716aee51
resource11 <mcmahon@resource11.com>
2015-11-19 22:44:09 -0500

    learning about how to use http file server functionality through node
    The pipe method is new knowledge. Not sure why we don't need to handle a request to get a correct solution here, must explore this more.

---
+++ learnyounode/10-http-file-server.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+'use strict';
+
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, { 'content-type': 'text/plain' });
+  fs.createReadStream(process.argv[3]).pipe(response);
+});
+server.listen(process.argv[2]);
+
