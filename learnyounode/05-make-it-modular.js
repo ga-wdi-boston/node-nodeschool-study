507b0a1fb8cb0cb1bb0d4117f6596a9f62f7ac37
EShimel <edieshimel@gmail.com>
2015-11-20 06:05:21 -0500

    Had the correct code, but not the correct file structrure.  Putting it all in one file doesn't make it very modular.

--- learnyounode/05-make-it-modular.js
+++ learnyounode/05-make-it-modular.js
@@ -140,13 +140,4 @@ var pathio = "." + ext;
 
     });
 };
-var mod = require('./05-make-it-modular.js');
-mod(process.argv[2], process.argv[3], function (err, listf) {
-    if (err) {
-        console.log('Error!');
-    } else {
-        for (var i = 0; i < listf.length; i++) {
-            console.log(listf[i]);
-        }
-    }
-});
+
