aeb09a5e88e13aa454774bcb27812ef435256cd9
kelvinma <me@kelvinma.com>
2015-11-22 14:02:12 -0500

    Completed Expressworks Lesson 3.
    Read bodyparser documentation.

--- expressworks/express.js
+++ expressworks/express.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var bodyparser = require('body-parser')
 
+app.use(bodyparser.urlencoded({extended: false}));
 app.use(express.static(process.argv[3] || path.join(__dirname, 'public')));
 
 app.set('view engine', 'jade');
@@ -9,6 +11,11 @@ app.set('views', process.argv[3]);
 
 app.get('/home', function(req, res) {
   res.render('index', {date: new Date().toDateString()});
+});
+
+app.post('/form', function(req, res){
+
+  res.send(req.body.str.split('').reverse().join(''));
 });
 
 app.listen(process.argv[2]);
