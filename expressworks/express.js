2640583130bbf39f0b40c644b6d520c19d0b56c1
kelvinma <me@kelvinma.com>
2015-11-22 14:11:38 -0500

    Completed Expressworks Exercise 05.
    Getting the hang of this.

--- expressworks/express.js
+++ expressworks/express.js
@@ -15,8 +15,14 @@ app.get('/home', function(req, res) {
 });
 
 app.post('/form', function(req, res){
-
   res.send(req.body.str.split('').reverse().join(''));
 });
+
+app.put('/message/:id', function(req, res){
+      res.send(require('crypto')
+      .createHash('sha1')
+      .update(new Date().toDateString() + req.params.id)
+      .digest('hex'));
+})
 
 app.listen(process.argv[2]);
