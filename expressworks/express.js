63079d6c29092aca7e87bac4adf0966371a63645
kelvinma <me@kelvinma.com>
2015-11-22 14:06:06 -0500

    Completed Expressworks 04.

--- expressworks/express.js
+++ expressworks/express.js
@@ -5,6 +5,7 @@ var bodyparser = require('body-parser')
 
 app.use(bodyparser.urlencoded({extended: false}));
 app.use(express.static(process.argv[3] || path.join(__dirname, 'public')));
+app.use(require('stylus').middleware('/path/to/*.styl' ))
 
 app.set('view engine', 'jade');
 app.set('views', process.argv[3]);
