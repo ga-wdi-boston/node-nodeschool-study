a754048b6df56fcd26e021825748cedf54332e6b
msyao-github <msyaois@gmail.com>
2016-03-03 17:08:55 -0500

    completed my first io

---
+++ learnyounode/response/02-my-first-io.js
@@ -0,0 +1,12 @@
+var fs = require('fs');
+
+var buf = fs.readFileSync(process.argv[2]).toString().split('\n').length-1;
+
+console.log(buf);
+
+
+// var fs = require('fs')
+//
+//      var contents = fs.readFileSync(process.argv[2])
+//      var lines = contents.toString().split('\n').length - 1
+//      console.log(lines)
