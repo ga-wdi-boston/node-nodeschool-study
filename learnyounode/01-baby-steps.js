3140f7f1a1b08b0ff5dde6e435abc2f0fb742c54
nwilcox11 <wilcox.nicholas.m@gmail.com>
2016-03-03 16:31:40 -0500

    You can access command-line arguments via the
    global process object

---
+++ learnyounode/01-baby-steps.js
@@ -0,0 +1,13 @@
+'use strict';
+
+//console.log(process.argv);
+
+
+  let sum = 0;
+    for (let i = 2; i < process.argv.length; i++) {
+      sum += Number(process.argv[i]);
+  }
+
+
+
+console.log(sum);
