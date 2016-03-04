7bb3fa18faca2acfc7d3a61e2469a401e211abc2
Dumplingpuff <jason.liang2012@gmail.com>
2016-03-04 14:38:38 -0500

    Update README and daily commit.

---
+++ learnyounode/04-filtered-ls.js
@@ -0,0 +1,12 @@
+'use strict';
+let fs = require('fs');
+
+// fs.readdir(prcess.argv[2], function(err, list) {
+//   list.forEach(function(e) {
+//     if e.extname(e) == process.argv[3] {
+//       process.e
+//     }
+//   });
+// });
+
+console.log();
