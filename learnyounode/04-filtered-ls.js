5bf3379994411af7b14893e8b494ac6e66ab0981
jpfriedman18 <jpfriedman18@gmail.com>
2016-03-03 21:50:06 -0500

    Up to 05

--- learnyounode/04-filtered-ls.js
+++ learnyounode/04-filtered-ls.js
@@ -5,11 +5,9 @@ let dir = process.argv[2];
 let type = "." + process.argv[3];
 
 fs.readdir(dir, function(err, files) {
-  let correctFiles = [];
-  for (let i = 0; i = files.length; i ++){
-    if (path.extname(files[i]) === type){
-      correctFiles.push(files[i]);
+  files.forEach(function(file){
+    if (path.extname(file) === type){
+      console.log(file);
     }
-  }
-  console.log(correctFiles);
+  });
 });
