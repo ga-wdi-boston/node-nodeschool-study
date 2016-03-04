7cc18a6373447b85f252a42af0f2b3313edb44e3
msyao-github <msyaois@gmail.com>
2016-03-04 13:35:26 -0500

    unfinished learnyounode study

--- learnyounode/response/02-my-first-io.js
+++ learnyounode/response/02-my-first-io.js
@@ -7,6 +7,6 @@ console.log(buf);
 
 // var fs = require('fs')
 //
-//      var contents = fs.readFileSync(process.argv[2])
-//      var lines = contents.toString().split('\n').length - 1
-//      console.log(lines)
+// var contents = fs.readFileSync(process.argv[2])
+// var lines = contents.toString().split('\n').length - 1
+// console.log(lines)
--- learnyounode/response/03-my-first-async-io.js
+++ learnyounode/response/03-my-first-async-io.js
@@ -7,10 +7,10 @@ fs.readFile(process.argv[2], 'utf8', function(err, data) {
 
 
 // var fs = require('fs')
-//     var file = process.argv[2]
+// var file = process.argv[2]
 //
-//     fs.readFile(file, function (err, contents) {
-//       // fs.readFile(file, 'utf8', callback) can also be used
-//       var lines = contents.toString().split('\n').length - 1
-//       console.log(lines)
-//     })
+//fs.readFile(file, function (err, contents) {
+//  // fs.readFile(file, 'utf8', callback) can also be used
+//  var lines = contents.toString().split('\n').length - 1
+//  console.log(lines)
+//})
