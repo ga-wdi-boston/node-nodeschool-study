82132e028319e1f13b84551c36ed8470ddb40a91
msyao-github <msyaois@gmail.com>
2016-03-05 17:30:16 -0500

    deleted extra files, moved response files to root

---
+++ learnyounode/07-http-collect.js
@@ -0,0 +1,26 @@
+var http = require('http')
+, concatStream = require('concat-stream')
+, url = process.argv[2];
+
+http.get(url, function (response) {
+  response.setEncoding('utf8');
+  response.pipe(concatStream(function (data) {
+    console.log(data.length);
+    console.log(data);
+
+  }));
+});
+
+
+// var http = require('http')
+//     var bl = require('bl')
+//
+//     http.get(process.argv[2], function (response) {
+//       response.pipe(bl(function (err, data) {
+//         if (err)
+//           return console.error(err)
+//         data = data.toString()
+//         console.log(data.length)
+//         console.log(data)
+//       }))
+//     })
