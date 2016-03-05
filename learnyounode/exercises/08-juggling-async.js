42b35f7483d881e1c34c039c7939c05fc5b98fbd
Celena T <chibiynm@gmail.com>
2016-03-05 16:59:32 -0500

    Edit 08

--- learnyounode/exercises/08-juggling-async.js
+++ learnyounode/exercises/08-juggling-async.js
@@ -5,15 +5,25 @@ let http = require('http');
 let result = [];
 let count = 0;
 
-for (let i = 2; i < process.argv.length; i++) {
-  http.get(process.argv[i], function (response) {
+let print = function() {
+  for (let i = 0; i < 3; i++) {
+    console.log(result[i]);
+  }
+}
+
+function httpGet(i) {
+  http.get(process.argv[2 + i], function (response) {
     response.pipe(bl(function (err, data) {
       result[i] = data.toString();
       count++;
 
       if (count === 3) {
-        console.log(result[i]);
+        print();
       }
     }))
   })
+}
+
+for (let i = 0; i < 3; i++) {
+  httpGet(i)
 }
