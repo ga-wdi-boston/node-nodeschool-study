5b27dada198d64e92fbcdb3f94b85bc808d2127f
Ethan <erdesi90562@gmail.com>
2016-03-07 07:46:45 -0500

    create a get request using node
    set encoding to utf8

---
+++ learnyounode/06-http-client.js
@@ -0,0 +1,12 @@
+var http = require('http');
+var url = process.argv[2];
+
+http.get(url, (response) => {
+  response.setEncoding('utf8');
+  response.on("data", (data) => {
+    console.log(data);
+  });
+})
+.on('error', (e) => {
+  console.log(`Got error: ${e.message}`);
+});
