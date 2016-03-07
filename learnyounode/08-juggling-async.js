1468781e999aaed29b1269227fe439068ce48114
RobertRuttenberg <rdruttenberg@gmail.com>
2016-03-06 23:11:43 -0500

    08 solution

---
+++ learnyounode/08-juggling-async.js
@@ -0,0 +1,35 @@
+var http = require('http');
+
+var urls = process.argv.slice(2);
+var results = [];
+
+// initialise results array
+for (i = 0; i < urls.length; i++) {
+  results[i] = null;
+}
+
+for (i = 0; i < urls.length; i++) {
+  (function(i) {
+    http.get(urls[i], function(request) {
+      var result = "";
+      request.setEncoding("utf8");
+      request.on("data", function(data) {
+        result += data;
+      });
+      request.on("end", function() {
+        results[i] = result;
+        var resultCount = 0;
+        for (j = 0; j < results.length; j++) {
+          if (results[j] != null) resultCount++;
+        }
+        if (resultCount == results.length) {
+          for (j = 0; j < results.length; j++) {
+            console.log(results[j]);
+          }
+        }
+      });
+    });
+  })(i);
+}
+
+//This one gave me trouble. Solution from https://github.com/frasertweedale/learnyounode-solutions/blob/master/http_juggle.js
