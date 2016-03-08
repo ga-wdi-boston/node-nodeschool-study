da06d4611408dd3cc410053917402990314bd3c0
jpfriedman18 <jpfriedman18@gmail.com>
2016-03-07 21:37:49 -0500

    Used videos linked in instructions to help

---
+++ expressworks/param-pam-pam.js
@@ -0,0 +1,14 @@
+'use strict';
+let express = require('express');
+let app = express();
+
+app.put('/message/:id', function(req, res){
+  let id = req.params.id;
+  let str = require('crypto')
+      .createHash('sha1')
+      .update(new Date().toDateString() + id)
+      .digest('hex');
+  res.end(str);
+});
+
+app.listen(process.argv[2]);
