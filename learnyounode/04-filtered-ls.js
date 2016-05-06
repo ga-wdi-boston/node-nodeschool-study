eba7b5894e8d8bbd6c6bbc51e5f8cd29d5fe662a
MichaelB167 <michael.m.bates@gmail.com>
2016-05-06 10:35:21 -0400

    Change file to data for semantics

--- learnyounode/04-filtered-ls.js
+++ learnyounode/04-filtered-ls.js
@@ -3,12 +3,10 @@
 let fs = require('fs');
 let path = require('path');
 
-fs.readdir(process.argv[2], function(err, files){
+fs.readdir(process.argv[2], function(err, data){
   files.forEach(function(file) {
     if(path.extname(file) === '.' + process.argv[3]) {
       console.log(file)
     }
   });
 });
-
-//not working yet, will try again tomorrow
