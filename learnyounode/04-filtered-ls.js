566e11c0ab2812769b1a48d0a1ae88b337668dc7
MichaelB167 <michael.m.bates@gmail.com>
2016-05-05 21:55:29 -0400

    Complete learnyounode exercise 4, googling helped

---
+++ learnyounode/04-filtered-ls.js
@@ -0,0 +1,12 @@
+'use strict';
+
+let fs = require('fs');
+let path = require('path');
+
+fs.readdir(process.argv[2], function(err, files){
+  files.forEach(function(file) {
+    if(path.extname(file) === '.' + process.argv[3]) {
+      console.log(file)
+    }
+  });
+});
