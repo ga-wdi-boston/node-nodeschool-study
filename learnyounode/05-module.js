961e34cfb8fef54cfb9b660935bcbfb4f5c6e04e
MichaelB167 <michael.m.bates@gmail.com>
2016-05-08 13:46:34 -0400

    Add missing curly braces to if statement

--- learnyounode/05-module.js
+++ learnyounode/05-module.js
@@ -5,8 +5,9 @@ let path = require('path')
 
 module.exports = fs.readdir(dirName, ext, function (err, list) {
   list.forEach(function (file) {
-    if (path.extname(file) === '.' + ext)
+    if (path.extname(file) === '.' + ext) {
       console.log(file);
+    }
   });
 });
 
