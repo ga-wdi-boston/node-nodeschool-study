59c9453e6b29d0d20178f9124660d2532d94eb95
OllieBuilds <scarborough.oliver@gmail.com>
2016-05-08 17:10:49 -0400

    Updates lyn and htn progress
    Progress on lyn 07 and htn

--- learnyounode/06-http-client.js
+++ learnyounode/06-http-client.js
@@ -2,8 +2,8 @@
 
 let http = require('http');
 
-http.get(process.argv[2], function(response) {
-  response.setEncoding('utf8');
-  response.on('error', console.error());
-  response.on('data', console.log());
+http.get(process.argv[2], function(request) {
+  request.setEncoding('utf8');
+  request.on('error', console.error(error));
+  request.on('data', console.log(data));
 });
