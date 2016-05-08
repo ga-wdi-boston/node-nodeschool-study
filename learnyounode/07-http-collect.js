90c416ae1716a1efc190d47444214127df46462f
“Benjamski” <benjadamski@gmail.com>
2016-05-08 18:41:33 -0400

    Some not working

--- learnyounode/07-http-collect.js
+++ learnyounode/07-http-collect.js
@@ -1,15 +1,12 @@
 var http = require('http');
+var bl = require('bl');
 
-var url = process.argv[2];
-
-http.get(url, function(request) {
-  var result = "";
-  request.setEncoding("utf8");
-  request.on("data", function(data) {
-    result += data;
-  });
-  request.on("end", function() {
-    console.log(result.length);
-    console.log(result);
-  });
+http.get(process.argv[2], function (response) {
+  response.pipe(bl(function (err, data) {
+    if (err)
+      return console.error(err);
+    data = data.toString();
+    console.log(data.length);
+    console.log(data);
+  }));
 });
---
+++ learnyounode/10-http-file-server.js
@@ -0,0 +1,8 @@
+var http = require('http'),
+    fs = require('fs');
+
+var server = http.createServer(function (req, res) {
+  var file = fs.createReadStream(process.argv[3]);
+  file.pipe(res);
+});
+server.listen(process.argv[2]);
