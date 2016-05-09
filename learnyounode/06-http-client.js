eb63de10551a15761a728de64296beb2e931cb2e
cuprous <zachary.simpson.1@gmail.com>
2016-05-08 23:02:58 -0400

    Complete learnyounode http GET problem
    Made the tests green.

---
+++ learnyounode/06-http-client.js
@@ -0,0 +1,10 @@
+'use strict';
+
+let http = require('http');
+
+http.get(process.argv[2], function(res) {
+  res.setEncoding("utf8")
+  res.on('data', function(data) {
+    console.log(data);
+  });
+});
