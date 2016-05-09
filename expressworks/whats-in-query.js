07c31a9397762a13355df1089c5561d09a3c4d4a
natdjerf <nadjerf@gmail.com>
2016-05-09 19:12:38 -0400

    Respose 05-07
    07 was meh.

---
+++ expressworks/whats-in-query.js
@@ -0,0 +1,11 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+
+var app = express()
+
+
+app.get('/search', function(req, res){
+  res.send(req.query)
+  })
+
+app.listen(process.argv[2])
