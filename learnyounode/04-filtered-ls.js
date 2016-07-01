e8a90e931db7d1520f786c2b876f435f1427be20
andreasantarl <andrea.santarl@gmail.com>
2016-06-30 22:00:22 -0400

    Response to #4
    WRONG

---
+++ learnyounode/04-filtered-ls.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var fs = require("fs");
+
+// let junk = fs.readdir((process.argv[2]), function (err, data) {
+//     if (err) return console.error(err);
+//     console.log(data.extname("txt"));
+// });
+
+// let junk = fs.readdir((process.argv[2]), data.extname("txt"));
+//     console.log(junk);
+
+
+
+
+//
+// let junk = fs.readdir(path, function (err, data) {
+//     if (err) return console.error(err);
+//     console.log(data.extname("txt"));
+// });
+
+
+fs.readdir(process.argv[2], function (err, path.extname("txt")) {
+    if (err) return console.error(err);
+    console.log(list)
+});
+
+// .extname('txt')
