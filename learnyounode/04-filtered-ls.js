32992f22d59830c50f95e1e65f6f01c48f42fe4c
modularjon <modularjon@gmail.com>
2016-07-01 08:42:11 -0400

    remove useless comments

--- learnyounode/04-filtered-ls.js
+++ learnyounode/04-filtered-ls.js
@@ -5,13 +5,10 @@ const path = require('path');
 
 fs.readdir(process.argv[2], function (err, list) {
   if (err) return console.error(err);
-  // let print = '';
 
   for (let i = 0, max = list.length; i < max; i++) {
     if (path.extname(list[i]) === ('.' + process.argv[3])) {
       console.log(list[i]);
     }
   }
-
-  // console.log(print);
 });
