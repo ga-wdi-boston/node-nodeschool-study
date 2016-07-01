f4d5b3e2e019ed8b88b0e4b78d8d0bcdf79d35fc
andreasantarl <andrea.santarl@gmail.com>
2016-06-30 21:09:37 -0400

    Response to #3
    This sucked...

--- learnyounode/02-my-first-io.js
+++ learnyounode/02-my-first-io.js
@@ -1,18 +1,29 @@
-'use strict';
-
+// 'use strict';
+//
 // var fs = require("fs");
 //
-// fs.toString();
+// // fs.toString();
+// //
+// // fs.readFileSync(process.argv[2]);
+// //
+// // var str = buf.toString().split("\n");
+// //
+// // console.log(str);
 //
-// fs.readFileSync();
 //
-// var str = buf.toString().split("\n");
+// console.log(fs.readFileSync(process.argv[2]).toString());
+// // var fs = require('fs');
+// // var array = fs.readFileSync(process.argv[2]).toString().split("\n");
+// // for(i in array) {
+// //     console.log(array[i]);
+// // // }
 //
-// console.log(str);
+// var fs = require('fs');
+// var array = fs.readFileSync(process.argv[2]).toString().split("\n");
+// for(i in array) {
+//     console.log(array[i]);
+// }
 
-
-var fs = require('fs');
-var array = fs.readFileSync().toString().split("\n");
-for(i in array) {
-    console.log(array[i]);
-}
+'use strict';
+const fs = require('fs');
+console.log(fs.readFileSync(process.argv[2]).toString().split("\n").length-1);
