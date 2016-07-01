3d5d47088b8364cd4a101a2ffae5f9f1e9aeb0b3
modularjon <modularjon@gmail.com>
2016-06-30 21:40:04 -0400

    Refactor code for exercise 1
    remove unnecessary function declarations

--- learnyounode/01-baby-steps.js
+++ learnyounode/01-baby-steps.js
@@ -1,13 +1,9 @@
 'use strict';
 
-const sumCommandLine = function() {
-  let sum = 0;
+let sum = 0;
 
-  for (let i = 2, max = process.argv.length; i < max; i++) {
-    sum += Number(process.argv[i]);
-  }
+for (let i = 2, max = process.argv.length; i < max; i++) {
+  sum += Number(process.argv[i]);
+}
 
-  return sum;
-};
-
-console.log(sumCommandLine());
+console.log(sum);
