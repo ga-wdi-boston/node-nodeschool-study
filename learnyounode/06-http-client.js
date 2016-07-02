ff6a5bad4758d7b191f769aae5284f6cc4e1d672
modularjon <modularjon@gmail.com>
2016-07-02 13:35:16 -0400

    Complete exercise 06 (7th)
    response.setEncoding('utf8') lets you skip getting a buffer object and gets
    strings

---
+++ learnyounode/06-http-client.js
@@ -0,0 +1,8 @@
+
+const http = require('http');
+
+http.get(process.argv[2], function (response) {
+  response.setEncoding('utf8')
+  response.on('error', console.error)
+  response.on('data', console.log)
+})
