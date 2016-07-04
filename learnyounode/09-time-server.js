cfd35dde949c38796a90e04443dcca14af1e4651
andreasantarl <andrea.santarl@gmail.com>
2016-07-04 17:35:39 -0400

    Responses to #9
    FInished

---
+++ learnyounode/09-time-server.js
@@ -0,0 +1,18 @@
+'use strict';
+
+let net = require('net');
+let date = new Date();
+
+let server = net.createServer(function (socket) {
+  if ((date.getMonth()+1<10)&&(date.getDate()+1<10)) {
+    let timestring = date.getFullYear()+"-0"+ (date.getMonth()+1) +"-0"+date.getDate()+" "+date.getHours()+":"+date.getMinutes()+"\n"
+    socket.write(timestring)
+    socket.end()
+  }else{
+    let timestring = date.getFullYear()+"-"+ (date.getMonth()+1) +"-"+date.getDate()+" "+date.getHours()+":"+date.getMinutes()+"\n"
+    socket.write(timestring)
+    socket.end()
+  }
+})
+
+server.listen(process.argv[2])
