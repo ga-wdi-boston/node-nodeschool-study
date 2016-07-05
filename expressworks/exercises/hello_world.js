2e492fae919f1f027e116e61276154c4a644bfc7
RoobScoobs <iruben229@gmail.com>
2016-07-05 00:38:16 -0400

    Answer to hello world exercise
    This is how we can create an Express.js app on port 3000, that responds with
    a string on '/':
    
        var express = require('express')
        var app = express()
        app.get('/', function(req, res) {
          res.end('Hello World!')
        })
        app.listen(3000)

---
+++ expressworks/exercises/hello_world.js
@@ -0,0 +1,10 @@
+// Create an Express.js app that outputs "Hello World!" when somebody goes to /home.
+// The port number will be provided to you by expressworks as the first argument of
+// the application, i.e., process.argv[2]
+
+var express = require('express')
+var app = express()
+app.get('/home', function(req, res) {
+  res.end('Hello World!')
+})
+app.listen(process.argv[2])
