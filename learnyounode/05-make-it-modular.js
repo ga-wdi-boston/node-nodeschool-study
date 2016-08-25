2853186d8e19052d8df22ee679728114a2df9e83
dvdpwll <dvdpwll456@gmail.com>
2016-08-25 16:58:07 -0400

    finished learnyounode 05

--- learnyounode/05-make-it-modular.js
+++ learnyounode/05-make-it-modular.js
@@ -1,42 +1,20 @@
 'use strict';
-let fs = require('fs');
+
+const path = require('path');
 let mymodule = require('./05-module.js');
 
 let directory = process.argv[2];
 let extenstion = process.argv[3];
 
-function listFiles() {
-  fs.readdir(directory, function doneRead(err, list){
-    for (let i = 0; i < list.length; i++) {
-      // I can't seem to pass the err catch for some reason.
-      // if (err) {
-      //   return doneRead(err);
-      //   console.error('There was an error', err);
-      //   return;
-      // }
-      let tempArray = list[i].split('.');
-      if (tempArray[1] === extenstion) {
-        console.log(list[i]);
-      }
+
+function callback(error, list){
+    if (error) {
+      console.log(error);
     }
-  })
+    list.forEach(function(file){
+      console.log(file);
+    });
 }
 
-mymodule(directory, extenstion, listFiles);
 
-//
-//
-// var fs = require('fs')
-//      var path = require('path')
-//
-//      var folder = process.argv[2]
-//      var ext = '.' + process.argv[3]
-//
-//      fs.readdir(folder, function (err, files) {
-//        if (err) return console.error(err)
-//        files.forEach(function(file) {
-//            if (path.extname(file) === ext) {
-//                console.log(file)
-//            }
-//        })
-//      })
+mymodule(directory, extenstion, callback);
--- learnyounode/05-module.js
+++ learnyounode/05-module.js
@@ -1,5 +1,22 @@
 'use strict';
 
-module.exports = function (directory, extenstion, callback) {
-  callback();
-};
+const fs = require('fs');
+const path = require('path');
+
+function listFiles(filepath, extenstion, callback) {
+  fs.readdir(filepath, function(error, list){
+    if (error) {
+      return callback(error);
+    }
+    else {
+      list = list.filter(function(file){
+        if (path.extname(file) === '.' + extenstion) {
+          return true;
+        }
+      });
+      return callback(null, list);
+    }
+  });
+}
+
+module.exports = listFiles;
