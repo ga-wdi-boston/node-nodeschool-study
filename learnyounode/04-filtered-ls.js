bd30f3addfba731947b8d605f271810c63e0199d
gabescarbrough <gabescarbrough@gmail.com>
2016-08-24 22:37:09 -0400

    Complete Filtered LS
    Prints a list of files in a given directory filtered by the extension of the files.

---
+++ learnyounode/04-filtered-ls.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+fs.readdir(process.argv[2], function (err, list) {
+  list.forEach(function (filename) {
+    if (path.extname(filename) === '.' + process.argv[3]) {
+      console.log(filename);
+    }
+  });
+});
