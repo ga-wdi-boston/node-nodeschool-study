1a7ab36fb2f3ca7a6ad22aa5a36435813e9b2c7f
natGalynker <natalgal@gmail.com>
2016-08-25 22:09:55 -0400

    Late commit
    did this last night and it worked fine
    
    now I can't get the verify test to work, checked with code online and I can't seem to catch
    what I am doing differently. It also seems like a lot of people have issues with this one

--- LS.js
+++ LS.js
@@ -1,11 +1,10 @@
+
 var fs = require('fs');
 var path = process.argv[2];
-var extention = process[3];
+var extention = process.argv[3];
 fs.readdir(path, function (err, list) {
-  for (var i = 0; i < list.length; i++) {
-    if (path.extname(list[i]) === extension) {
-      console.log(list[i]);
-    }
-  }
-});
-  
+list.forEach(function(type) {
+    if (path.extname(type) === '.' + process.argv[3])
+      console.log(type);
+    });
+  });
