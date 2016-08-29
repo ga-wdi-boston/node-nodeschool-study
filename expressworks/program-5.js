6de74c3999489328cddd26e2f72a846a29a2b24b
jordanallain <jallain@gmail.com>
2016-08-28 23:34:22 -0400

    Complete problem 5 expressworks
    Use css file in problem 5 expressworks

---
+++ expressworks/program-5.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+app.use(require('stylus').middleware(process.argv[3] || 'public'));
+app.use(express.static(process.argv[3] || 'public'));
+
+app.listen(process.argv[2] || 3000);
