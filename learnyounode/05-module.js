a6e0b26d5a46a178ca11d37d76b19053178d71b4
gabescarbrough <gabescarbrough@gmail.com>
2016-08-29 21:00:49 -0400

    Complete 05 Make it Modular
    Now passes all tests

--- learnyounode/05-module.js
+++ learnyounode/05-module.js
@@ -7,10 +7,12 @@ const path = require('path');
 module.exports = function (dirName, fileExt, callback) {
   fs.readdir(dirName, function (err, list) {
     if (err) return callback(err);
+    let results = [];
     list.forEach(function (filename) {
       if (path.extname(filename) === '.' + fileExt) {
-        console.log(filename);
+        results.push(filename);
       }
     });
+    return callback(null, results);
   });
 };
