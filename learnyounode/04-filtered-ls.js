fe08e0f9ccbfba816838bcfae9953e7a8919b6cb
rebekahheacock <rebekah.heacock@gmail.com>
2016-10-06 21:20:28 -0400

    04 filtered ls
    Move variable declaration for ext outside of forEach

--- learnyounode/04-filtered-ls.js
+++ learnyounode/04-filtered-ls.js
@@ -6,8 +6,8 @@ var fs = require('fs');
 var path = require('path');
 
 fs.readdir(process.argv[2], function (err, files) {
-  files.forEach(function (file) {
-    var ext = process.argv[3];
+  var ext = process.argv[3];
+  files.forEach(function (file) {  
     if (path.extname(file).substring(1) === ext) {
       console.log(file);
     }
