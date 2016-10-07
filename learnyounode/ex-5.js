51a0f2c58a85bd60c0947bad11185d9a597f70b1
james-coombs <coombsj4@yahoo.com>
2016-10-07 08:58:36 -0400

    Begin work on ex 5
    Start ex 5.

---
+++ learnyounode/ex-5.js
@@ -0,0 +1,16 @@
+'use strict';
+
+let fs = require('fs');
+
+var path = require('path')
+
+var folder = process.argv[2]
+
+fs.readdir(folder, function (err, data) {
+  if (err) {
+    return console.log(err);
+  }
+  data.for
+
+/// something
+});
