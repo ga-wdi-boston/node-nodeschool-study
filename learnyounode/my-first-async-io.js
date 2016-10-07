3d0170396958286ed7863928ac8ad49d4eb518a7
kopiusmaximus <alexander.kopynec@gmail.com>
2016-10-06 23:08:30 -0400

    Complete learnyounode exercise 4
    Complete learnyounode exercise 4: My First Async I/O

---
+++ learnyounode/my-first-async-io.js
@@ -0,0 +1,27 @@
+'use strict';
+
+let fs = require('fs');
+
+let filePath = process.argv[2];
+
+let buf;
+
+function processData(data){
+  let numLines = data.split('\n').length - 1;
+  console.log(numLines);
+}
+
+function read(err, data) {
+  if (err) {
+    throw err;
+  }
+  processData(data);
+}
+
+fs.readFile(filePath, 'utf8', read);
+
+// console.log('getBuffer returns', getBuffer());
+
+// let numLines = buf.toString().split('\n').length - 1;
+//
+// console.log(numLines);
