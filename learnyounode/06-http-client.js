40d054d50960ff6bf8b3b6786268ee459eeb27cd
“coffeeisamazing” <caleb.searles@libertymutual.com>
2016-12-07 20:33:19 -0500

    Learned how to use http get.

---
+++ learnyounode/06-http-client.js
@@ -0,0 +1,12 @@
+/*
+Write a program that performs an HTTP GET request to a URL provided to you
+as the first command-line argument. Write the String contents of each
+"data" event from the response to a new line on the console (stdout).
+*/
+
+var http = require('http');
+http.get(process.argv[2], function (response) {
+  response.setEncoding('utf8');
+  response.on('data', console.log);
+  response.on('error', console.error);
+}).on('error', console.error);
