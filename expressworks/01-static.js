32de92e87242833fd7040d1f5fb235ce67829dea
kibs86 <kibs086@gmail.com>
2016-12-08 16:26:06 -0500

    Completed Static - Exercise 2 of 8
    Created an Express.js app that serves the static content at the
    path provided.

---
+++ expressworks/01-static.js
@@ -0,0 +1,8 @@
+// Create an Express.js app that serves static assets.
+
+var path = require('path');
+var express = require('express');
+
+var app = express();
+app.use(express.static(process.argv[3] || path.join(__dirname, 'public')));
+app.listen(process.argv[2]);
