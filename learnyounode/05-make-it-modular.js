467598bec8f545845e82974dda3391d11810f21d
callmelife <matthew.g.sommers@gmail.com>
2016-12-07 23:02:58 -0500

    Really difficult stuff, again I had to do a lot of googling to get through this. Will this be covered in class? Clarity: 2, Comfort: 2

--- learnyounode/05-make-it-modular.js
+++ learnyounode/05-make-it-modular.js
@@ -1,9 +1,9 @@
-var contentFile = require('./contentFile');
+var lslib = require('./lslib');
 
 var dirname = process.argv[2];
 var ext = process.argv[3];
 
-contentFile(dirname, ext, function(err, files) {
+lslib(dirname, ext, function(err, files) {
   for (i = 0; i < files.length; i++) {
     console.log(files[i]);
   }
