34500d22a2efafdb942c7966ba172fc737d175c4
kibs86 <kibs086@gmail.com>
2016-12-08 16:19:37 -0500

    Completed Hello World - Exercise 1 of 8
    Created an Express.js app that outputs "Hello World!" when somebody
    goes to /home.
    
    I learned how to create a basic Express.js app.

---
+++ expressworks/00-hello-world.js
@@ -0,0 +1,15 @@
+// Create an Express.js app that outputs "Hello World!" when somebody goes to /home.
+
+// The port number will be provided to you by expressworks as the first argument of
+// the application, i.e., process.argv[2].
+
+// Run $ killall node  before verifying exercises (in your terminal on Mac OS X)
+// to end any previous processes.
+
+var express = require('express')
+
+var app = express()
+app.get('/home', function(req, res) {
+  res.end('Hello World!')
+});
+app.listen(process.argv[2]);
