ffc79d357ef4b1969b5c8ed5ef9146788c1239a4
ashlenprice <ashlen.price@gmail.com>
2017-02-26 18:03:22 -0500

    Complete  async and filtered

---
+++ learnyounode/03-my-first-async-io.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+const file = process.argv[2];
+
+ fs.readFile(file, function (err, contents) {
+   if (err) {
+     return console.log(err);
+   }
+
+let lines = contents.toString().split('\n').length - 1;
+console.log(lines);
+
+});
