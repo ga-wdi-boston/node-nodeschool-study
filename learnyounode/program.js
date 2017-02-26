54611a303094e205a498e7f472b07b745e11613d
“wilsonkaya” <fatihkaya2@yahoo.com>
2017-02-26 13:23:55 -0500

    I have understood how console.log works on node

--- learnyounode/program.js
+++ learnyounode/program.js
@@ -1,9 +1,1 @@
-
-var fs = require('fs');
-var buff = fs.readFileSync(process.argv[2]);
-var str = buff.toString().split('\n');
-var num = str.length;
-
-
-
-console.log(num -1);
+console.log("HELLO WORLD")
