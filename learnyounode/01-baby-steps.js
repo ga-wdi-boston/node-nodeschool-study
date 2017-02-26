0df4e3d3b8f425773b06cf8908849e7110e0cd1a
ggwilliams <gabithan@icloud.com>
2017-02-26 13:55:28 -0500

    learnyounode exercies 00-05

---
+++ learnyounode/01-baby-steps.js
@@ -0,0 +1,9 @@
+// console.log(process.argv);
+// Number(process.argv[2]);
+let result = 0
+
+for (let i = 2; i < process.argv.length; i++) {
+  result += Number(process.argv[i])
+}
+
+console.log(result);
---
+++ learnyounode/04-filtered-ls.js
@@ -0,0 +1,25 @@
+let fs = require('fs')
+let path = require('path')
+
+fs.readdir(process.argv[2], function (err, list) {
+  list.forEach(function (file) {
+    if (path.extname(file) === '.' + process.argv[3])
+    console.log(file)
+  })
+})
+
+// OFFICIAL SOLUTION:
+// var fs = require('fs')
+// var path = require('path')
+//
+// var folder = process.argv[2]
+// var ext = '.' + process.argv[3]
+//
+// fs.readdir(folder, function (err, files) {
+//   if (err) return console.error(err)
+//   files.forEach(function (file) {
+//     if (path.extname(file) === ext) {
+//       console.log(file)
+//     }
+//   })
+// })
