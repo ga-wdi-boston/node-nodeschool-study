6ec26b5ba6f4df6a8462c9a9089a4102e4fb45ff
kinkorea <alexkarasik89@gmail.com>
2017-02-26 19:38:32 -0500

    My First Async IO

--- program.js
+++ program.js
@@ -1,5 +1,8 @@
-var fs = require('fs');
 
-var contents = fs.readFileSync(process.argv[2]);
-var lines = contents.toString().split('\n').length - 1;
-console.log(lines);
+var fs = require('fs');
+var file = process.argv[2];
+
+fs.readFile(file, function (err, contents){
+  var lines = contents.toString().split('\n').length - 1;
+  console.log(lines);
+});
