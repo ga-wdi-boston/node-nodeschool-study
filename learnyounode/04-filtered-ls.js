d45dc060445e5b33ffda690092c0a1182b6a09ec
“newayliu1” <newayliu1@gmail.com>
2017-02-26 19:05:13 -0500

    Finish make it modular
    - I still do not how to use the call back function in module

--- learnyounode/04-filtered-ls.js
+++ learnyounode/04-filtered-ls.js
@@ -1,10 +1,13 @@
 var fs = require('fs');
 const path = require('path');
 
-fs.readdir(process.argv[2],(_err, list) => {
+fs.readdir(process.argv[2],(err, list) => {
   // console.log(list.filter((el) => path.extname(el)===".md"));
   list.forEach((el) => {
-    if (path.extname(el)===".md")
+    if (err){
+    return callback(err);
+    }
+    if (path.extname(el)==="."+process.argv[3])
     console.log(el);});
 });
 //
---
+++ learnyounode/05-make-it-modular.js
@@ -0,0 +1,13 @@
+
+var mymodule = require('./mymodule');
+var dir = process.argv[2];
+var filterStr = process.argv[3];
+
+mymodule(dir, filterStr, function (err, list) {
+  if (err)
+    return console.error('There was an error:', err);
+
+  list.forEach(function (file) {
+    console.log(file);
+  });
+});
