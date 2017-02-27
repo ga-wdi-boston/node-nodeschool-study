3485ee4e6e9d5733712cab6d1015b21626ec6e4d
“wilsonkaya” <fatihkaya2@yahoo.com>
2017-02-27 18:39:58 -0500

    I am not capable of doing 8th and 9th of the study

--- learnyounode/program.js
+++ learnyounode/program.js
@@ -1,17 +1,7 @@
+var http = require('http');
+var bl = require('bl');
 
-var module = require('mymodule.js');
-var first = process.argv[2];
-var second = '.' + process.argv[3];
-// console.log(second)
-
-fs.readdir(first, (err, data) => {
-  var list = data;
-  // console.log(list)
-  list.forEach(function(word) {
-    if (word.includes(second)) {
-      console.log(word);
-    }
-
-  });
+http.get(process.argv[2], function (response){
+  response.pipe(function())
 
 });
