705ce086ffde2467ff1b8bbfc6119eaa2e6596a3
lbreguet <louis-w@live.com>
2017-02-27 20:47:58 -0500

    HTTP client
    -learned how to make an http request using node
    -learned how to parse data from a request using node

--- learnyounode/04-filtered-ls.js
+++ learnyounode/04-filtered-ls.js
@@ -4,9 +4,11 @@ var path = require('path');
 var folder = process.argv[2];
 var ext = '.' + process.argv[3];
 
-fs.readdir(folder, function (err, files) {
-  if (err) return console.error(err);
-  files.forEach(function (file) {
+fs.readdir(folder, function(err, files) {
+  if (err) {
+    return console.error(err);
+  }
+  files.forEach(function(file) {
     if (path.extname(file) === ext) {
       console.log(file);
     }
