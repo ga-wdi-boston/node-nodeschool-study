388377b2e41def747dd3e97e55261202eb065810
mario7746 <m.kennedykavouras@gmail.com>
2017-02-28 08:44:11 -0500

    I started this last night and did alot of reading. Still after 2 hours I was having major trouble with these exercises. I dont't know if its the instructions or that i'm just not understanding but I decided to send it when i have and go from there.

--- learnyounode/04-filtered-ls.js
+++ learnyounode/04-filtered-ls.js
@@ -2,4 +2,11 @@ let fs = require('fs');
 let path = require('path');
 
 fs.readdir(process.argv[2], function (err, list){
+  if(err) {
+    console.error(error.stack);
+    return;
+  }
+  list.forEach(function (list) {
+    console.log(path.extname(list));
+  });
 });
