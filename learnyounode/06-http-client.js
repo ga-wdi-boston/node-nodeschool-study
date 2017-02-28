32c6dcdaa6fd4fb225ba677569412d456ab0355f
yourUsername <your_email@example.com>
2017-02-27 22:07:13 -0500

    Created module module requires modules path and fs to export module add arguments to function

---
+++ learnyounode/06-http-client.js
@@ -0,0 +1,12 @@
+let filterFn = require('./solution_filter.js');
+let dir = process.argv[2];
+let filterStr = process.argv[3];
+
+filterFn(dir, filterStr, function (err, list) {
+  if (err)
+    return console.error('There was an error:', err);
+
+  list.forEach(function (file) {
+    console.log(file);
+  });
+});
