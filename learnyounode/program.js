f04d1ae2b4c9d2ffe46433d7a362ef7d21edc468
skylarkJ <janaryndin@gmail.com>
2017-02-28 19:30:20 -0500

    Change the second part

--- learnyounode/program.js
+++ learnyounode/program.js
@@ -1,9 +1,27 @@
-var fs = require('fs')
-var path = require('path')
+var http = require('http')
+var bl = require('bl')
+var results = []
+var count = 0
 
-fs.readdir(process.argv[2], function (err, list) {
-  list.forEach(function (file) {
-    if (path.extname(file) === '.' + process.argv[3])
-      console.log(file)
+function printResults () {
+  for (var i = 0; i < 3; i++)
+    console.log(results[i])
+}
+
+function httpGet (index) {
+  http.get(process.argv[2 + index], function (response) {
+    response.pipe(bl(function (err, data) {
+      if (err)
+        return console.error(err)
+
+      results[index] = data.toString()
+      count++
+
+      if (count == 3) // yay! we are the last one!
+        printResults()
+    }))
   })
-})
+}
+
+for (var i = 0; i < 3; i++)
+  httpGet(i)
