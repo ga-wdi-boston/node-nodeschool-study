e93ad9ea4fc076e27a810321af1ca5d4904c3be0
lbreguet <louis-w@live.com>
2017-03-01 16:21:49 -0500

    Exercise complete
    -parsing json using a post request
    -install and require an npm module called `body parser`

---
+++ expressworks/03-good-old-form.js
@@ -0,0 +1,10 @@
+var bodyparser = require('body-parser');
+var express = require('express');
+var app = express();
+app.use(bodyparser.urlencoded({
+  extended: false
+}));
+app.post('/form', function(req, res){
+  res.send(req.body.str.split('').reverse().join(''));
+});
+app.listen(process.argv[2]);
