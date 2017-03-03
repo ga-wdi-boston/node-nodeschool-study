597fe39a32fcec22020c6d49b7dd15f68c0350a4
“wilsonkaya” <fatihkaya2@yahoo.com>
2017-03-02 22:31:17 -0500

    I am getting close but I need to ask a tutor to fix the last bug

---
+++ make-it-modular2/module.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+var answer = function (dirName,extName, callback){
+  fs.readdir(dirName, (err, data) => {
+      if(err){
+        return callback(err);
+      }
+
+      data.forEach(function(word){
+       if(path.extname(word) === "."+ extName)
+        callbackcd (word);
+      });
+    });
+
+};
+
+module.exports = answer;
