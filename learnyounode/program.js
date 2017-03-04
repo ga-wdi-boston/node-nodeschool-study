5e635ec14229f96b199ea8bc8267b99b6a524c10
“wilsonkaya” <fatihkaya2@yahoo.com>
2017-03-04 18:45:44 -0500

    Last solution

--- learnyounode/program.js
+++ learnyounode/program.js
@@ -1,12 +1,24 @@
-const answer = require('./module');
+const fs = require('fs');
+const http = require('http');
+var net = require('net')
+let path1 = process.argv[2];
+let date = new Date();
 
-let data = process.argv[2];
-let extention = process.argv[3];
-const print = function(word){
-  console.log(word)
-}
+let year = date.getFullYear();
+let month = date.getMonth() + 1;
+let day = date.getDate();
+let hour = date.getHours();
+let minute = date.getMinutes();
 
-answer(data, extention, print);
+let wholeYear = year + "-" +"0"+ month + "-" +"0"+day + " " + hour + ":" + minute
 
 
-// console.log(result);
+
+
+
+
+  var server = net.createServer(function (socket) {
+    let data = wholeYear;
+    socket.end(data + '\n');
+  });
+  server.listen(path1);
