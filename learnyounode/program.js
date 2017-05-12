71461cdc4ee9150632de220bdb9a6613c3e39231
xiangcatherine <xiang.catherine@gmail.com>
2017-05-11 21:06:32 -0400

    Completed exercise 1 and 2
    You can access command-line arguments using the process object, which has
    an argv property (which is an array containing the complete command
    line). Also, you can coerce strings into numbers by passing it to
    Number().

---
+++ learnyounode/program.js
@@ -0,0 +1,14 @@
+'use strict'
+
+// const sayHello = function () {
+//   console.log('HELLO WORLD')
+// }
+//
+// sayHello()
+
+// let result = 0
+//
+// for (let i = 2; i < process.argv.length; i++)
+//   result += Number(process.argv[i])
+//
+// console.log(result)
